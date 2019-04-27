d5750b3090f8e0936e0909aef8feeabf25fbc96f
TriDivide <liamsmitha@gmail.com>
2019-04-27 12:35:22 +0100

    - wrote the singleton pattern for the PlayerModel

---
+++ LD44/Assets/Scripts/PlayerModel.cs
@@ -0,0 +1,14 @@
+﻿
+//using UnityEngine;
+
+public class PlayerModel {
+    
+    private static PlayerModel _instance;
+
+    public static PlayerModel Instance() {
+        if (_instance == null) {
+            _instance = new PlayerModel();
+        }
+        return _instance;
+    }
+}
