43063308415a3708e24106a7f010fd053a8ffd2d
TriDivide <liamsmitha@gmail.com>
2019-04-27 10:40:16 +0100

    - started work on the player motor for movement and rotation

---
+++ LD44/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour {
+
+    [SerializeField]
+    private Component player;
+
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start() {
+        rb = player.GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 movementVelocity) {
+        velocity = movementVelocity;
+    }
+
+    public void Rotate(Vector3 rotationVelocity) {
+        rotation = rotationVelocity;
+    }
+
+    private void FixedUpdate() {
+        PerformMovement();
+        PerformRotation();
+    }
+
+    private void PerformMovement() {
+
+    }
+
+    private void PerformRotation() {
+
+    }
+}
